8ec0ee3aea1c035705cb0789b77f9fc1e20dee6a
Kim_Jeong_Yeop <59815869+20171601@users.noreply.github.com>
2021-12-20 18:39:48 +0900

    28P14

---
+++ Assignment/28P14.ino
@@ -0,0 +1,222 @@
+#include <Servo.h>
+
+/////////////////////////////
+// Configurable parameters //
+/////////////////////////////
+
+// Arduino pin assignment
+#define PIN_LED 9            // [20200000] LED핀 설정
+#define PIN_SERVO 10         //[20191979] Servo 핀 설정
+#define PIN_IR A0            // [20191979] 적외선 핀(아날로그핀)
+
+// Framework setting
+#define _DIST_TARGET 255    // [20213085] 정지하려는 위치 목표값
+#define _DIST_MIN 100         //[20191979] 최소거리
+#define _DIST_MAX 410         //[20191979] 최대거리
+
+// Distance sensor
+#define _DIST_ALPHA 0.5 //[20203118] DIST_ALPHA 값 설정
+
+// Servo range
+#define _DUTY_MIN 1200    //[3148]  서보의 가동 최소 각도(0)
+#define _DUTY_NEU 1350      //[3150] servo neutral position (90 degree)
+#define _DUTY_MAX 1400                // [3169] 서보의 최대 가동 각도(180º)
+
+// Servo speed control
+#define _SERVO_ANGLE 15 //[20203118] 최대 가동범위에 따른 목표 서보 회전각
+#define _SERVO_SPEED 30 //[20203118] 서보 속도 설정
+
+// Event periods
+#define _INTERVAL_DIST 30   // [20213099] 거리 센서 주기 설정
+#define _INTERVAL_SERVO 20  // [20213099] 서보 주기 설정
+#define _INTERVAL_SERIAL 100  // [20213099] 시리얼 표시 주기 설정
+
+// PID parameters
+#define _KP 1.5      // [3158] 비례상수 설정
+#define _KD 65.0      // [3159] 미분제어 상수
+#define _KI 0.007     //[3157] 적분제어 상수
+#define INTERVAL 20
+
+#define   // DELAY_MICROS * samples_num^2 의 값이 최종 거리측정 인터벌임. 넉넉하게 30ms 잡음.
+#define DELAY_MICROS  1500 // 필터에 넣을 샘플값을 측정하는 딜레이(고정값!)
+#define EMA_ALPHA 0.13     // EMA 필터 값을 결정하는 ALPHA 값. 작성자가 생각하는 최적값임.
+float ema_dist=0;            // EMA 필터에 사용할 변수
+float filtered_dist;       // 최종 측정된 거리값을 넣을 변수. loop()안에 filtered_dist = filtered_ir_distance(); 형태로 사용하면 됨.
+float samples_num = 3;     // 스파이크 제거를 위한 부분필터에 샘플을 몇개 측정할 것인지. 3개로 충분함! 가능하면 수정하지 말 것.
+
+
+
+
+//////////////////////
+// global variables //
+//////////////////////
+
+// Servo instance
+Servo myservo;   // [20213090] 서보 인스턴스 선언
+
+// Distance sensor
+float dist_target; // location to send the ball 
+float dist_raw, dist_ema; //[20203118] 거리와 노이즈 필터 적용 후 거리를 저장하기 위한 변수
+
+// Event periods
+unsigned long last_sampling_time_dist, last_sampling_time_servo, last_sampling_time_serial; //[20203118] 마지막으로 측정한 거리, 마지막으로 측정한 서보 각도(각 이벤트별로 업데이트를 위해 측정하는 시간)
+bool event_dist, event_servo, event_serial; //[20203118] 이벤트 별로 이번 루프에서 업데이트 여부
+
+// Servo speed control
+int duty_chg_per_interval; //[20213086]interval 당 최대 duty 변화량
+int duty_target, duty_curr; //[20213086] 목표 duty와 현재 duty 값
+
+
+// PID variables
+float error_curr, error_prev, control, pterm, dterm, iterm; // 비례 제어를 위한 전에 측정한 오차, 새로 측정한 오차 값, 비례항, 적분항, 미분항 변수
+int a,b;
+
+
+
+float under_noise_filter(void){ // 아래로 떨어지는 형태의 스파이크를 제거해주는 필터
+  int currReading;
+  int largestReading = 0;
+  for (int i = 0; i < samples_num; i++) {
+    currReading = 100 + 300.0 / (b - a) * (ir_distance() - a);
+    if (currReading > largestReading) { largestReading = currReading; }
+    // Delay a short time before taking another reading
+    delayMicroseconds(DELAY_MICROS);
+  }
+  return largestReading;
+}
+
+float filtered_ir_distance(void){ // 아래로 떨어지는 형태의 스파이크를 제거 후, 위로 치솟는 스파이크를 제거하고 EMA필터를 적용함.
+  // under_noise_filter를 통과한 값을 upper_nosie_filter에 넣어 최종 값이 나옴.
+  int currReading;
+  int lowestReading = 1024;
+  for (int i = 0; i < samples_num; i++) {
+    currReading = under_noise_filter();
+    if (currReading < lowestReading) { lowestReading = currReading; }
+  }
+  // eam 필터 추가
+  ema_dist = EMA_ALPHA*lowestReading + (1-EMA_ALPHA)*ema_dist;
+  return ema_dist;
+}
+//===================================================
+
+
+void setup() {
+  iterm = 0;
+  dterm = 0;
+  pterm = 0;
+// initialize GPIO pins for LED and attach servo 
+  pinMode(PIN_LED, OUTPUT);  
+  myservo.attach(PIN_SERVO);  
+// initialize global variables
+  last_sampling_time_dist = last_sampling_time_servo = last_sampling_time_serial = 0; // [3155] 샘플링 시각 기록 변수 초기화
+  event_dist = event_servo = event_serial = false;  
+  dist_target = _DIST_TARGET; 
+
+
+// move servo to neutral position
+  myservo.writeMicroseconds(_DUTY_NEU); 
+// initialize serial port
+    Serial.begin(57600);                          
+// convert angle speed into duty change per interval.
+  duty_chg_per_interval = int((_DUTY_MAX - _DUTY_MIN) * (_SERVO_SPEED /  _SERVO_ANGLE) * (float(INTERVAL) / 1000.0));   //[3159] 서보 각속도에 맞추어 센서 인식 간격 변경? [3150] 서보의 각속도를 원하는 Angle로 나누어 interval을 설정한다.
+
+  a = 74;
+  b = 360;
+
+
+}
+float ir_distance(void){ // return value unit: mm
+  float val;
+  float volt = float(analogRead(PIN_IR));
+  val = ((6762.0/(volt-9.0))-4.0) * 10.0;
+  return val;
+}
+
+
+void loop() {
+  float raw_dist = ir_distance();
+  float dist_cali = 100 + 300.0 / (b - a) * (raw_dist - a);
+  dist_ema = filtered_ir_distance() + 2.8;
+
+/////////////////////
+// Event generator // [
+/////////////////////
+unsigned long time_curr = millis();
+  if(time_curr >= last_sampling_time_dist + _INTERVAL_DIST) {
+        last_sampling_time_dist += _INTERVAL_DIST;
+        event_dist = true;
+  }
+  if(time_curr >= last_sampling_time_servo + _INTERVAL_SERVO) {
+        last_sampling_time_servo += _INTERVAL_SERVO;
+        event_servo = true;
+  }
+  if(time_curr >= last_sampling_time_serial + _INTERVAL_SERIAL) {
+        last_sampling_time_serial += _INTERVAL_SERIAL;
+        event_serial = true;
+  }
+
+
+////////////////////
+// Event handlers //
+////////////////////
+
+  if(event_dist) {
+     event_dist = false;
+  // get a distance reading from the distance sensor
+      filtered_dist = ema_dist;  
+
+  // PID control logic
+    error_curr = _DIST_TARGET - filtered_dist; 
+    dterm = _KD*(error_curr-error_prev);
+    pterm = _KP*error_curr;
+    iterm += _KI * error_curr;  
+    control = pterm + dterm + iterm;          
+
+  // duty_target = f(duty_neutral, control)
+    duty_target = control + 1250;
+
+  // keep duty_target value within the range of [_DUTY_MIN, _DUTY_MAX]
+    if (duty_target < _DUTY_MIN) {
+      duty_target = _DUTY_MIN;  // duty_target < _DUTY_MIN 일 때 duty_target 를 _DUTY_MIN 로 고정
+    } else if (duty_target > _DUTY_MAX) {
+      duty_target = _DUTY_MAX; // duty_target > _DUTY_MAX 일 때 duty_target 를 _DUTY_MAX 로 고정
+    }  // [3153] [_DUTY_MIN, _DUTY_MAX] 로 서보의 가동범위를 고정하기 위한 최소한의 안전장치
+    error_prev = error_curr;
+  }
+
+  if(event_servo) {
+    event_servo = false;  //[20203118] 
+    // adjust duty_curr toward duty_target by duty_chg_per_interval
+    if(duty_target > duty_curr) {
+      duty_curr += duty_chg_per_interval;
+      if(duty_curr > duty_target) duty_curr = duty_target;
+    }  
+    else {
+      duty_curr -= duty_chg_per_interval;
+      if(duty_curr < duty_target) duty_curr = duty_target;
+    }      //[20213086]
+
+
+    // update servo position
+    myservo.writeMicroseconds(duty_curr);   //[20213086]
+  }
+
+  if(event_serial) {
+    event_serial = false; 
+    Serial.print("IR:");
+    Serial.print(ema_dist);
+    Serial.print(",T:");
+    Serial.print(dist_target);
+    Serial.print(",P:");
+    Serial.print(map(pterm,-1000,1000,510,610));
+    Serial.print(",D:");
+    Serial.print(map(dterm,-1000,1000,510,610));
+    Serial.print(",I:");
+    Serial.print(map(iterm,-1000,1000,510,610));
+    Serial.print(",DTT:");
+    Serial.print(map(duty_target,1000,2000,410,510));
+    Serial.print(",DTC:");
+    Serial.print(map(duty_curr,1000,2000,410,510));
+    Serial.println(",-G:245,+G:265,m:0,M:800");
+  }
+}
